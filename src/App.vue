5e4444b47ff3c960cd46b3d394789ccb61ea4d07
apostol <admin@dpankratov.ru>
2017-11-24 00:09:15 +0400

    Init

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app id="app">
+    <v-navigation-drawer
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <main id="main" app>
+      <router-view v-title="title"></router-view>
+    </main>
+    <v-footer fixed app>
+      <v-spacer></v-spacer>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        items: [{
+          icon: 'bubble_chart',
+          title: 'Inspire'
+        }],
+        title: 'Izhevsk Community'
+      }
+    }
+  }
+
+</script>
+<style>
+  #main {
+    height: 100vh
+  }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-carousel hide-controls lazy>
+    <v-carousel-item v-for="(item,i) in items" v-bind:src="item.src" :key="i"></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      title: 'Izhevsk Community',
+      items: [
+        {
+          src: '/static/city/1.jpg'
+        },
+        {
+          src: '/static/city/2.jpg'
+        },
+        {
+          src: '/static/city/3.jpg'
+        },
+        {
+          src: '/static/city/4.jpg'
+        },
+        {
+          src: '/static/city/5.jpg'
+        },
+        {
+          src: '/static/city/6.jpg'
+        },
+        {
+          src: '/static/city/7.jpg'
+        },
+        {
+          src: '/static/city/8.jpg'
+        },
+        {
+          src: '/static/city/9.jpg'
+        },
+        {
+          src: '/static/city/10.jpg'
+        },
+        {
+          src: '/static/city/11.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.carousel
+{
+  height:100%
+}
+</style>
